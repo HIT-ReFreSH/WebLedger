2b9690eac105d72c06674d908bad2a431ce82da7
orange-q <2258774026@qq.com>
2023-11-14 20:55:28 +0800

    EditableLedgder组件功能的基本实现

--- fronted/src/components/EditableLedgder/index.jsx
+++ fronted/src/components/EditableLedgder/index.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Form, Input } from 'antd-mobile'
+import { Button, Space } from 'antd-mobile'
 
 export default function EditableLedgder(props) {
     // 该组件用于账单的编辑
@@ -7,10 +9,32 @@ export default function EditableLedgder(props) {
         defaultVaule = {},
         getValue = () => { }
     } = props;
-    const [title, setTitle] = useState(defaultVaule.title || '');
+    const [category, setCategory] = useState(defaultVaule.category || '')
     const [type, setType] = useState(defaultVaule.type || '');
-    const [num, setNum] = useState(defaultVaule.num || '');
+    const [amount, setAmount] = useState(defaultVaule.amount || '');
+    const [time, setTime] = useState(defaultVaule.time || '');
     return (
-        <div>{num}</div>
+        <div>
+            <Form layout='horizontal'>
+                <Form.Item label='类别' name='category'>
+                    <Input placeholder='请输入收支物品类别' clearable onChange={(value) => { setCategory(value) }} />
+                </Form.Item>
+                <Form.Item label='类型' name='type'>
+                    <Input placeholder='请输入收支物品具体类型' clearable onChange={(value) => { setType(value) }} />
+                </Form.Item>
+                <Form.Item label='金额' name='amount'>
+                    <Input placeholder='请输入金额，收入为正数，支出为负数' clearable onChange={(value) => { setAmount(value) }} />
+                </Form.Item>
+                <Form.Item label='时间' name='time'>
+                    <Input placeholder='请按下回车键入当前时间' clearable onChange={(value) => { setTime(value) }} onEnterPress={(e) => {
+                        const date = new Date()
+                        e.target.value = date.toLocaleString()
+                    }} />
+                </Form.Item>
+            </Form>
+            <Button block color='success' size='large' onClick={() => { console.log(category, type, amount, time); }}>
+                提交账单
+            </Button>
+        </div>
     )
 }
--- fronted/src/pages/LedgerCreate/index.jsx
+++ fronted/src/pages/LedgerCreate/index.jsx
@@ -1,7 +1,13 @@
+import EditableLedgder from '../../components/EditableLedgder';
+
 export default function LedgerCreate() {
     return (
         <div>
-            <h1>LedgerCreate</h1>
+            <div>
+                <h1>LedgerCreate</h1>
+            </div>
+
+            <EditableLedgder />
         </div>
     );
 }
