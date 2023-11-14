7c36d382a6fd2757e962cbb1bdde89bc54dca371
chenFan <2513151911@qq.com>
2023-11-14 19:07:41 +0800

    update:Echart

--- fronted/src/components/EditableLedgder/index.jsx
+++ fronted/src/components/EditableLedgder/index.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 
-export default function EditableLedgder(props){
-    const {defaultVaule}=props;
-    const [value,setValue]=useState(defaultVaule);
+export default function EditableLedgder(props) {
+    // 该组件用于账单的编辑
+    // 传入默认值，同时传入回调函数，用于获取编辑后的值 
+    const {
+        defaultVaule = {},
+        getValue = () => { }
+    } = props;
+    const [title, setTitle] = useState(defaultVaule.title || '');
+    const [type, setType] = useState(defaultVaule.type || '');
+    const [num, setNum] = useState(defaultVaule.num || '');
     return (
-        <div>edit</div>
+        <div>{num}</div>
     )
 }
